744e19a74e04a532806cf053d8fc37de56f0dada
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2013-03-03 21:43:49 +0200

    my first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,26 @@
+class HomeController < ApplicationController
+
+  def index
+  end
+
+  def news
+
+  end
+
+  def page
+
+  end
+
+  def post
+
+  end
+
+  def archive
+
+  end
+
+  def gallery
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tatilplanim::Application.config.secret_token = '8be9dc92590f092691cfa548b66d237df20b3b3201188fddebce512149b678d7f55f5b943b833b3d47b23b9fca15a552e46219d26f74f8247187c0344451486c'
